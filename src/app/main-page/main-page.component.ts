c2aa7315585285d1364ab064f6c420411793e444
Jens Natzschka <jens.natzschka@posteo.de>
2025-01-17 16:27:07 +0100

    first draft of message components

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { MessageComponent } from './message/message.component';
+import { Message } from '../models/message';
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [MessageComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
 
+  messages: Message[] = [];
+
+  constructor() {
+    this.messages = [
+      { id: '1', content: 'Hello, how are you?', timestamp: new Date(1737047891), isOwn: true, userId: '1' },
+      { id: '2', content: 'I am fine, thank you!', timestamp: new Date(1737047891), isOwn: false, userId: '2' },
+    ];
+  }
 }
---
+++ src/app/main-page/message/message.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AvatarComponent } from '../../shared/avatar/avatar.component';
+import { Message } from '../../models/message';
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [CommonModule, AvatarComponent],
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
+})
+export class MessageComponent implements OnInit {
+  @Input() message!: Message;
+  isOwn: boolean;
+  content: string;
+  timestamp: Date;
+  userId: string;
+
+  constructor() {
+    this.isOwn = false;
+    this.content = '';
+    this.timestamp = new Date();
+    this.userId = '';
+  }
+
+  ngOnInit() {
+    this.isOwn = this.message.isOwn;
+    this.content = this.message.content;
+    this.timestamp = this.message.timestamp;
+    this.userId = this.message.userId;
+  }
+}
