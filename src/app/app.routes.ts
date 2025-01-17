19e546d300d7dec4515916451bdd8695f31218db
Jens Natzschka <jens.natzschka@posteo.de>
2025-01-17 11:18:43 +0100

    added first basic compontent routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticationComponent
+  },
+  {
+    path: 'chat',
+    component: MainPageComponent
+  },
+  {
+    path: 'imprint',
+    component: ImprintComponent
+  }
+];
