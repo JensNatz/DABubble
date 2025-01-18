fcfe06058e1aeadee74f7e30590afff8c05f28fe
AndrejYaotsin <andrej.butalov@gmx.de>
2025-01-18 20:32:54 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
+
 export const routes: Routes = [
   {
     path: '',
