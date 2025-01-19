aebd93d3e9b4082d692574017ab71c0ef5a5e3e4
Stephan <stephan-zager@web.de>
2025-01-19 20:54:03 +0100

    fixed login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -7,7 +7,7 @@ import { InputFieldComponent } from "../shared/input-field/input-field.component
   standalone: true,
   imports: [InputFieldComponent],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss', '../shared/scss/btn-scss.scss']
+  styleUrls: ['./login.component.scss', '../shared/scss/btn.scss','../shared/scss/input-field.scss',]
 })
 export class LoginComponent {
 onSubmit: any;
