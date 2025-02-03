24d2c09e9e3882cc9f4bb7e8b260cc8246fe8d8e
AndrejYaotsin <andrej.butalov@gmx.de>
2025-02-03 13:01:24 +0100

    channel edit component open close window

--- src/app/shared/channel-edit/channel-edit.component.ts
+++ src/app/shared/channel-edit/channel-edit.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-channel-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './channel-edit.component.html',
   styleUrl: './channel-edit.component.scss'
 })
-export class ChannelEditComponent {
 
+
+export class ChannelEditComponent {
+  nameEdit = false;
+
+  openNameEdit() {
+    this.nameEdit = true;
+  }
+
+  closeNameEdit() {
+    this.nameEdit = false;
+  }
 }
