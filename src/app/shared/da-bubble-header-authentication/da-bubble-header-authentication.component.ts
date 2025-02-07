21324d4f2b9f7f013246ad97c15724cfb2f94e96
Stephan <stephan-zager@web.de>
2025-02-07 19:01:57 +0100

    fixed logo  header

--- src/app/shared/da-bubble-header-authentication/da-bubble-header-authentication.component.ts
+++ src/app/shared/da-bubble-header-authentication/da-bubble-header-authentication.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-da-bubble-header-authentication',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './da-bubble-header-authentication.component.html',
   styleUrl: './da-bubble-header-authentication.component.scss'
 })
