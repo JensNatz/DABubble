fe2291ba1cbdce18e29bb0cad7e99488ec4861fa
AndrejYaotsin <andrej.butalov@gmx.de>
2025-02-19 09:33:20 +0100

    profil_edit scss responsive

--- src/app/shared/menu-profil/menu-profil.component.ts
+++ src/app/shared/menu-profil/menu-profil.component.ts
@@ -1,15 +1,5 @@
 import { Component, inject, Input } from '@angular/core';
-import { ChannelServiceService } from '../../services/firebase-services/channel-service.service';
-import { Subscription } from 'rxjs';
 import { LoginService } from '../../services/firebase-services/login-service';
-import { UserServiceService } from '../../services/firebase-services/user-service.service';
-import { User } from '../../models/user';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { SearchService } from '../../services/search.service';
-import { take } from 'rxjs/operators';
-import { AddUserToChannelComponent } from '../add-user-to-channel/add-user-to-channel.component';
-import { ModalService } from '../../services/modal.service';
 import { Router } from '@angular/router';
 
 @Component({
