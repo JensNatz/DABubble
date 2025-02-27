e0ee7c01ae6273b5d70d5c18873cebbfc517a8db
AndrejYaotsin <andrej.butalov@gmx.de>
2025-02-27 17:06:27 +0100

    img bugfix

--- src/app/authentication/avatar/avatar.component.ts
+++ src/app/authentication/avatar/avatar.component.ts
@@ -29,12 +29,12 @@ export class AvatarComponent {
   registrationSuccessful: boolean = false;
 
   avatarImages = {
-    '1': '../../../assets/img/avatar1.svg',
-    '2': '../../../assets/img/avatar2.svg',
-    '3': '../../../assets/img/avatar3.svg',
-    '4': '../../../assets/img/avatar4.svg',
-    '5': '../../../assets/img/avatar5.svg',
-    '6': '../../../assets/img/avatar6.svg'
+    '1': 'assets/img/avatar1.svg',
+    '2': 'assets/img/avatar2.svg',
+    '3': 'assets/img/avatar3.svg',
+    '4': 'assets/img/avatar4.svg',
+    '5': 'assets/img/avatar5.svg',
+    '6': 'assets/img/avatar6.svg'
   }
 
 
